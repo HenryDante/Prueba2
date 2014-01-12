86ab7b4b6861dc053fdba1be9f9d9f35c7b1b551
HenryDante <henryalejan10@hotmail.com>
2014-01-12 01:21:25 -0600

    todo funciona bien

--- index.php
+++ index.php
@@ -20,5 +20,6 @@
 	<h3>Modificandolo desde el default</h3>
 	<p>En este nueva actualizacion hablaremos un poco de....</p>
 	<h4>Ahorita esta funcionando todo a la perfeccion</h4>
+	<h1>VAYA TODO FUNCIONA BIEN HASTA AHORITA</h1>
 </body>
 </html>
