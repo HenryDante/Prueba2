0d801987f5fa349b7b2873524316bfaccff1760f
HenryDante <henryalejan10@hotmail.com>
2014-01-12 00:16:04 -0600

    nuevo cambio en la master

--- index.php
+++ index.php
@@ -15,6 +15,6 @@
 	<p>Modificando de nuevo el archivo</p>
 	<h2>Modificandolo desde la master</h2>
 	<h3>Modificandolo desde el default</h3>
-
+	<p>En este nueva actualizacion hablaremos un poco de....</p>
 </body>
 </html>
