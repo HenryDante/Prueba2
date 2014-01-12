7e2621f4e26feacb5b3137d81c2b030e078bf195
HenryDante <henryalejan10@hotmail.com>
2014-01-12 00:31:02 -0600

    Fondo de color a la etiquete <p>

--- index.php
+++ index.php
@@ -9,16 +9,15 @@
 	h1{
 		background-color: blue;
 	}
+	p{
+		background-color: silver;
+	}
 </style>
 <body>
 	<h1>Bienvenido al mundo GitHub</h1>
 	<p>Modificando de nuevo el archivo</p>
 	<h2>Modificandolo desde la master</h2>
 	<h3>Modificandolo desde el default</h3>
-<<<<<<< HEAD
 	<p>En este nueva actualizacion hablaremos un poco de....</p>
-=======
-	<h1>Creando una nueva actualizacion</h1>
->>>>>>> default
 </body>
 </html>
