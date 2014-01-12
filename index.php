e012ab931bfc5faf1c3472abf699a3f2f37ca2e3
HenryDante <henryalejan10@hotmail.com>
2014-01-12 11:13:39 -0600

    Creando un nuevo commit de div

--- index.php
+++ index.php
@@ -14,13 +14,15 @@
 	}
 </style>
 <body>
-	<h1>Bienvenido al mundo GitHub</h1>
+	<div>
+		<h1>Bienvenido al mundo GitHub</h1>
 	<p>Modificando de nuevo el archivo</p>
 	<h2>Modificandolo desde la master</h2>
 	<h3>Modificandolo desde el default</h3>
 	<p>En este nueva actualizacion hablaremos un poco de....</p>
 	<h4>Ahorita esta funcionando todo a la perfeccion</h4>
 	<h3>Probemos haci a ver que tal</h3>
+	</div>
 	<div>
 		<p>Esto sera para default</p>
 	</div>
